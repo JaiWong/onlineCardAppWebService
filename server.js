d687be2af1f3c4cd430659fd298d20c9145813c9
Jairus Wong <24001954@myrp.edu.sg>
2026-01-09 13:08:32 +0800

    m

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.listen(port,()=>{
 app.get('/allcards',async(req,res)=>{
     try{
         let connection  = await mysql.createConnection(dbConfig);
-        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        const [rows] = await connection.execute('SELECT * FROM cards');
         res.json(rows);
     }catch (err) {
         console.log(err);
@@ -54,7 +54,7 @@ app.post('/addcard', async (req, res) => {
     }
 });
 // DELETE card
-app.delete('/cards/:id', async (req, res) => {
+app.delete('/deletecard/:id', async (req, res) => {
     const { id } = req.params;
 
     try {
